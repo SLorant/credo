7ccef445a389146f9243310e440176dd7dfbfbc8
SLorant <sutuslorant@gmail.com>
2026-02-18 08:10:31 +0100

    fix: icon paths

--- src/components/services/utils/cardData.ts
+++ src/components/services/utils/cardData.ts
@@ -4,41 +4,41 @@ const cardData = [
     title: "credo performance",
     items: [
       {
-        icon: "icons/list_white/performance1.svg",
+        icon: "/icons/list_white/performance1.svg",
         text: "Hirdetési, médiamegjelenési stratégia",
       },
       {
-        icon: "icons/list_white/performance2.svg",
+        icon: "/icons/list_white/performance2.svg",
         text: "PPC kampánykezelés",
       },
       {
-        icon: "icons/list_white/performance3.svg",
+        icon: "/icons/list_white/performance3.svg",
         text: "Hírlevél automatizáció, eDM stratégia",
       },
       {
-        icon: "icons/list_white/performance4.svg",
+        icon: "/icons/list_white/performance4.svg",
         text: "Keresőmarketing",
       },
       {
-        icon: "icons/list_white/performance5.svg",
+        icon: "/icons/list_white/performance5.svg",
         text: "Konverzióoptimalizálás",
       },
       {
-        icon: "icons/list_white/performance6.svg",
+        icon: "/icons/list_white/performance6.svg",
         text: "Enhanced Ecommerce beállítás",
       },
       {
-        icon: "icons/list_white/performance7.svg",
+        icon: "/icons/list_white/performance7.svg",
         text: "Auditálás és stratégiai tanácsadás",
       },
       {
-        icon: "icons/list_white/performance8.svg",
+        icon: "/icons/list_white/performance8.svg",
         text: "E-kereskedelem menedzsment",
       },
     ],
-    image: "illustrations/performance_bottom.webp",
-    imageTop: "illustrations/performance_top.webp",
-    mobileImage: "illustrations/performance_phone.webp",
+    image: "/illustrations/performance_bottom.webp",
+    imageTop: "/illustrations/performance_top.webp",
+    mobileImage: "/illustrations/performance_phone.webp",
     infotext: `Adatból Dunát lehet rekeszteni, tiszta következtetésből viszont kevés van. Mi a számok mögötti emberi viselkedést, fogyasztói magatartást térképezzük fel, hogy a büdzséd valós üzleti értéket teremtsen.`,
   },
   {
@@ -46,33 +46,33 @@ const cardData = [
     title: "credo creative",
     items: [
       {
-        icon: "icons/list_white/creative1.svg",
+        icon: "/icons/list_white/creative1.svg",
         text: "Arculattervezés, vizuális márkaépítés, brandbook kialakítása",
       },
       {
-        icon: "icons/list_white/creative2.svg",
+        icon: "/icons/list_white/creative2.svg",
         text: "Kreatív szövegírás",
       },
       {
-        icon: "icons/list_white/creative3.svg",
+        icon: "/icons/list_white/creative3.svg",
         text: "Online, offline grafikai tervezés",
       },
       {
-        icon: "icons/list_white/creative4.svg",
+        icon: "/icons/list_white/creative4.svg",
         text: "Közösségi média stratégia, tartalomkészítés, menedzsment",
       },
       {
-        icon: "icons/list_white/creative5.svg",
+        icon: "/icons/list_white/creative5.svg",
         text: "Reklámkampányok, hirdetési tartalmak kivitelezése",
       },
       {
-        icon: "icons/list_white/creative6.svg",
+        icon: "/icons/list_white/creative6.svg",
         text: "Kiadványszerkesztés, nyomdai előkészítés",
       },
     ],
-    image: "illustrations/creative_bottom.webp",
-    imageTop: "illustrations/creative_top.webp",
-    mobileImage: "illustrations/creative_phone.webp",
+    image: "/illustrations/creative_bottom.webp",
+    imageTop: "/illustrations/creative_top.webp",
+    mobileImage: "/illustrations/creative_phone.webp",
     infotext: `A fogyasztó érző ember, aki átlát a manipuláción. Kerüljük a dobozos ötleteket. Olyan kulturálisan releváns vizuális nyelvet alkotunk, ami párbeszédet indít a márkád és közönsége között.`,
   },
   {
@@ -80,33 +80,33 @@ const cardData = [
     title: "credo digital",
     items: [
       {
-        icon: "icons/list_white/digital1.svg",
+        icon: "/icons/list_white/digital1.svg",
         text: "Digitális UX/UI rendszerek tervezése  (felhasználói felület és élmény kialakítása)",
       },
       {
-        icon: "icons/list_white/digital2.svg",
+        icon: "/icons/list_white/digital2.svg",
         text: "Weboldal, landing page létrehozása",
       },
       {
-        icon: "icons/list_white/digital3.svg",
+        icon: "/icons/list_white/digital3.svg",
         text: "Webshop tervezés, kivitelezés",
       },
       {
-        icon: "icons/list_white/digital4.svg",
+        icon: "/icons/list_white/digital4.svg",
         text: "Weboldal és webshop karbantartás, üzemeltetés",
       },
       {
-        icon: "icons/list_white/digital5.svg",
+        icon: "/icons/list_white/digital5.svg",
         text: "Digitális analitika",
       },
       {
-        icon: "icons/list_white/digital6.svg",
+        icon: "/icons/list_white/digital6.svg",
         text: "Digitális PR",
       },
     ],
-    image: "illustrations/digital_bottom.webp",
-    imageTop: "illustrations/digital_top.webp",
-    mobileImage: "illustrations/digital_phone.webp",
+    image: "/illustrations/digital_bottom.webp",
+    imageTop: "/illustrations/digital_top.webp",
+    mobileImage: "/illustrations/digital_phone.webp",
     infotext: `Nem hagyjuk, hogy a digitális hitelesség növekedésed gátja legyen. Felhasználói élményre, bizalomépítésre kihegyezett rendszereket tervezünk, kivitelezünk, amelyek egyszerűen teszik a dolgukat: eladnak, konvertálnak.`,
   },
   {
@@ -114,33 +114,33 @@ const cardData = [
     title: "credo production",
     items: [
       {
-        icon: "icons/list_white/production1.svg",
+        icon: "/icons/list_white/production1.svg",
         text: "Short-form videók (kreatív tervezés, forgatás, vágás)",
       },
       {
-        icon: "icons/list_white/production2.svg",
+        icon: "/icons/list_white/production2.svg",
         text: "Szereplő vagy modell biztosítása",
       },
       {
-        icon: "icons/list_white/production3.svg",
+        icon: "/icons/list_white/production3.svg",
         text: "Imázsfotózás, modellfotózás, termékfotózás, mood jellegű tartalmak",
       },
       {
-        icon: "icons/list_white/production4.svg",
+        icon: "/icons/list_white/production4.svg",
         text: "Stúdió és on-location produkció",
       },
       {
-        icon: "icons/list_white/production5.svg",
+        icon: "/icons/list_white/production5.svg",
         text: "Videó-produkciós anyagok előállítása",
       },
       {
-        icon: "icons/list_white/production6.svg",
+        icon: "/icons/list_white/production6.svg",
         text: "Drónfelvételek, werk anyagok",
       },
     ],
-    image: "illustrations/production_bottom.webp",
-    imageTop: "illustrations/production_top.webp",
-    mobileImage: "illustrations/production_phone.webp",
+    image: "/illustrations/production_bottom.webp",
+    imageTop: "/illustrations/production_top.webp",
+    mobileImage: "/illustrations/production_phone.webp",
     infotext: `Marketinged üzemanyaga a tartalom, mi pedig biztosítjuk az utánpótlást. Fotó, videó, drónfelvétel… Házon belül gyártjuk le a teljes vizualitást, mert hiszünk abban, hogy a hitelesség az apró részletekben – a fényekben, a vágás ritmusában és a képkockák minőségében – dől el.`,
   },
 ];
